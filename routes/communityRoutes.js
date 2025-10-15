1d2bff0a813f58f4ee8441f1d87c52901993e7a6
Nikhil <nikhil17607@gmail.com>
2025-10-15 11:07:18 +0530

    SERVERSIDE CHANGES MADE

--- routes/communityRoutes.js
+++ routes/communityRoutes.js
@@ -1,14 +1,17 @@
 import express from 'express'
-import { createCommunity,getCommunity, getCommunityMessages, sendCommunityMessage } from '../controllers/communityController.js';
+import { createCommunity,getCommunity, getCommunityMessages, sendCommunityMessage, addCommunityMembers, removeCommunityMembers, listMyCommunities } from '../controllers/communityController.js';
 import auth from "../middleware/authMiddleware.js"
 
 
 const router=express.Router()
 
 router.post("/",auth,createCommunity)
-router.get('/:communityid',getCommunity)
-router.get('/:communityid/messages', getCommunityMessages)
+router.get('/my', auth, listMyCommunities)
+router.get('/:communityid', auth, getCommunity)
+router.get('/:communityid/messages', auth, getCommunityMessages)
 router.post('/:communityid/messages', auth, sendCommunityMessage)
+router.post('/:communityid/members/add', auth, addCommunityMembers)
+router.post('/:communityid/members/remove', auth, removeCommunityMembers)
 
 
 export default router;
